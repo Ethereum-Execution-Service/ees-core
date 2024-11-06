1acd581e7d75f069d6264fb00e3a2089aed46e9d
victorbrevig <victorbrevig@gmail.com>
2024-11-06 10:21:31 +0100

    JobRegistryHandler compile error fix

--- test/handlers/JobRegistryHandler.sol
+++ test/handlers/JobRegistryHandler.sol
@@ -5,14 +5,14 @@ import {Test} from "forge-std/src/Test.sol";
 import {TokenProvider} from "../utils/TokenProvider.sol";
 import {ICoordinator} from "../../src/interfaces/ICoordinator.sol";
 import {MockERC20} from "../mocks/MockERC20.sol";
-import {JobCreator} from "../actors/JobCreator.sol";
-import {JobSponsor} from "../actors/JobSponsor.sol";
+//import {JobCreator} from "../actors/JobCreator.sol";
+//import {JobSponsor} from "../actors/JobSponsor.sol";
 import {MockJobRegistry} from "../mocks/MockJobRegistry.sol";
 
 contract JobRegistryHandler is Test, TokenProvider {
     MockJobRegistry jobRegistry;
-    JobCreator[] public jobCreators;
-    JobSponsor[] public jobSponsors;
+    //JobCreator[] public jobCreators;
+    //JobSponsor[] public jobSponsors;
 
 
     constructor() {
